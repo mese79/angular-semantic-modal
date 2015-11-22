a9f1f4dd7ab008f05e4974b3702a8f20e8847950
mese79 <mese1979@gmail.com>
2015-11-22 19:49:00 +0330

    use promise instead of callback

--- semantic-modal.js
+++ semantic-modal.js
@@ -7,12 +7,15 @@ function getUUID() {
 
 var semanticModal = angular.module("SemanticModal", []);
 
+// Modal factory:
 semanticModal.factory("Modal", ["$rootScope", "$q", "$http", "$compile", "$controller", "$timeout", "$document",
 	function($rootScope, $q, $http, $compile, $controller, $timeout, $document) {
 	
 		function Modal(settings) {
 			var self = this;
 			
+			self.defered = $q.defer();
+
 			self.settings = angular.extend({
 				allowMultiple: true,
 				closable: false
@@ -39,14 +42,25 @@ semanticModal.factory("Modal", ["$rootScope", "$q", "$http", "$compile", "$contr
 				delete self.settings.controller;
 			}
 
+			if (self.settings.message)
+				self.scope.message = self.settings.message;
+
+			// Make functions for modal behaviors:
+			function onApprove() {
+				self.defered.resolve("approved");
+			}
+			function onDeny() {
+				self.defered.resolve("denied");
+			}
+
 			// Add and compile modal directive.
 			self.scope.options = {
 				"closable": self.settings.closable,
 				"allowMultiple": self.settings.allowMultiple,
-				"onVisible": ctrl.onVisible || ctrl[self.settings.onVisible],
-				"onHidden": ctrl.onHidden || ctrl[self.settings.onHidden],
-				"onApprove": ctrl.onApprove || ctrl[self.settings.onApprove],
-				"onDeny": ctrl.onDeny || ctrl[self.settings.onDeny]
+				"onVisible": ctrl.onVisible || ctrl[self.settings.onVisible] || self.settings.onVisible,
+				"onHidden": ctrl.onHidden || ctrl[self.settings.onHidden] || self.settings.onHidden,
+				"onApprove": onApprove,
+				"onDeny": onDeny
 			};
 			var angularElem = angular.element("<semantic-modal></semantic-modal>");
 			angularElem.attr({
@@ -61,7 +75,25 @@ semanticModal.factory("Modal", ["$rootScope", "$q", "$http", "$compile", "$contr
 
 		Modal.prototype.show = function() {
 			var self = this;
-			self.element.modal("show");
+			// Create new defer each time.
+			self.defered = $q.defer();
+
+			$timeout(function() {
+				self.element.modal("show");
+			}, 10);
+			
+			return self.defered.promise;
+		};
+
+		Modal.prototype.setMessage = function(msg) {
+			this.scope.message = msg;	
+		};
+
+		Modal.prototype.close = function() {
+			var self = this;
+			$timeout(function() {
+				self.element.modal("hide");
+			}, 10);
 		};
 
 		Modal.prototype.destroy = function() {
@@ -74,6 +106,7 @@ semanticModal.factory("Modal", ["$rootScope", "$q", "$http", "$compile", "$contr
 	}
 ]);
 
+// Modal directive:
 semanticModal.directive("semanticModal", function($document) {
 	return {
 
@@ -85,6 +118,86 @@ semanticModal.directive("semanticModal", function($document) {
 		},
 		link: function(scope, element, attrs) {
 			element.modal(scope.options);
+		}
+
+	};
+});
+
+// Dimmer factory:
+semanticModal.factory("Dimmer", ["$rootScope", "$q", "$http", "$compile", "$controller", "$timeout", "$document",
+	function($rootScope, $q, $http, $compile, $controller, $timeout, $document) {
+	
+		function Dimmer(settings) {
+			var self = this;
+			
+			self.settings = angular.extend({
+				parent: $document.find("body").eq(0)
+			}, settings);
+
+			var url = settings.template || settings.templateUrl;
+			if (!url) {
+				url = '<div class="ui dimmer"><div class="ui text loader"></div></div>';
+			}
+
+			self.id = getUUID();
+			
+			// Get scope from settings or create a new one.	
+			self.scope = (settings.scope || $rootScope).$new();
+			delete self.settings.scope;
+			
+			// Create a controller instance, if specified.
+			var ctrl = {};
+			if (self.settings.controller) {
+				ctrl = $controller(self.settings.controller, {
+					$scope: self.scope
+				});
+				delete self.settings.controller;
+			}
+
+			// Add and compile modal directive.
+			self.scope.options = {
+				"onShow": ctrl.onShow || ctrl[self.settings.onShow],
+				"onHide": ctrl.onHide || ctrl[self.settings.onHide],
+				"onChange": ctrl.onChange || ctrl[self.settings.onChange],
+				"closable": false
+			};
+			var angularElem = angular.element("<semantic-dimmer></semantic-dimmer>");
+			angularElem.attr({
+				"id": self.id,
+				"template-url": url,
+				"options": "options"
+			});
+			self.element = $compile(angularElem)(self.scope);
+			self.settings.parent.append(self.element);
+		};
+
+		Dimmer.prototype.show = function() {
+			var self = this;
+			self.element.dimmer("show");
+		};
+
+		Dimmer.prototype.hide = function() {
+			var self = this;
+			self.element.dimmer("hide");
+		};
+
+		return Dimmer;
+
+	}
+]);
+
+// Dimmer directive:
+semanticModal.directive("semanticDimmer", function($document) {
+	return {
+
+		restrict: "EA",
+		transclude: true,
+		replace: true,
+		templateUrl: function(element, attrs) {
+			return attrs.templateUrl;
+		},
+		link: function(scope, element, attrs) {
+			element.dimmer(scope.options);
 		}
 
 	};
